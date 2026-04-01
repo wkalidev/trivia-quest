5eb7ad6767f57e36649b4c87d5da8fd9c40a359c
wkalidev <wkalidev@gmail.com>
2026-04-01 09:26:18 +0200

    fix: replace next-pwa with ducanh2912

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {},
 };
 
 module.exports = withPWA(nextConfig);
